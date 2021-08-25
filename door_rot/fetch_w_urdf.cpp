1e9228bf598ef8711e857abb80260abeb62be622
servetb <servetbora9@hotmail.com>
2021-08-26 00:57:11 +0200

    -seg fault at sampling/solving, +motion kept together correctly

--- door_rot/fetch_w_urdf.cpp
+++ door_rot/fetch_w_urdf.cpp
@@ -1,3 +1,7 @@
+//
+// Created by serboba on 22.08.21.
+//
+
 #include <chrono>
 #include <thread>
 
@@ -68,8 +72,8 @@ int main(int argc, char **argv)
     // scene_dart->updateCollisionObject("myfirst",door_dart);
 
 
-    fetch_dart->setJoint("r_gripper_finger_joint", 0.03);
-    fetch_dart->setJoint("l_gripper_finger_joint", 0.03);
+    fetch_dart->setJoint("r_gripper_finger_joint", 0.05);
+    fetch_dart->setJoint("l_gripper_finger_joint", 0.05);
     //door_dart->setJoint("base_to_door",0.0);
 
 
@@ -83,7 +87,7 @@ int main(int argc, char **argv)
 
         darts::TSR::Specification start_spec;
         start_spec.setFrame(fetch_name, "wrist_roll_link", "base_link");
-        start_spec.setPose(0.27, -0.4, 1.26,  // goal pos 72+20
+        start_spec.setPose(0.78, -0.25, 1.26,  // goal pos 72+20
                            0.0, 0.71, 0.0, -0.71);
 
         darts::TSR start_tsr(world, start_spec);
@@ -96,11 +100,12 @@ int main(int argc, char **argv)
 
         darts::TSR::Specification goal_spec;
         goal_spec.setFrame(fetch_name, "wrist_roll_link", "base_link");
-        goal_spec.setPose(0.40, 0.27, 0.90,  // starting pos 72+60
+        goal_spec.setPose(0.40, 0.13, 0.90,  // starting pos 72+60
                           0.5, -0.5, 0.5, 0.5);
 
         auto goal_tsr = std::make_shared<darts::TSR>(world, goal_spec);
         auto goal = builder.getGoalTSR(goal_tsr);
+
         builder.setGoal(goal);
 
         auto rrt = std::make_shared<ompl::geometric::RRTConnect>(builder.info, true);
@@ -130,7 +135,7 @@ int main(int argc, char **argv)
 
         darts::TSR::Specification constraint_spec;
         constraint_spec.setFrame(fetch_name, "wrist_roll_link", "base_link");
-        constraint_spec.setPose(0.40, 0.27, 0.76,  // starting pos
+        constraint_spec.setPose(0.40, 0.13, 0.76,  // starting pos
                                 0.5, -0.5, 0.5, 0.5);
         constraint_spec.setNoZPosTolerance();
 
@@ -143,12 +148,13 @@ int main(int argc, char **argv)
 
         darts::TSR::Specification goal_spec;
         goal_spec.setFrame(fetch_name, "wrist_roll_link", "base_link");
-        goal_spec.setPose(0.40, 0.27, 0.76,  // starting pos
+        goal_spec.setPose(0.40, 0.13, 0.76,  // starting pos
                           0.5, -0.5, 0.5, 0.5);
 
 
         auto goal_tsr = std::make_shared<darts::TSR>(world, goal_spec);
         auto goal = builder.getGoalTSR(goal_tsr);
+
         builder.setGoal(goal);
 
         auto rrt = std::make_shared<ompl::geometric::RRTConnect>(builder.info, true);
@@ -174,49 +180,54 @@ int main(int argc, char **argv)
     const auto &plan_to_place = [&]() {
 
         darts::PlanBuilder builder(world);
-
-        auto door_joint = door_dart->getGroupJoint("dodoor","base_to_door").second;
-
-        auto door_frame = door_dart->getFrame("door");
-        auto sup = door_joint->getJointProperties();
-
-        //fetch_dart->reparentFreeFrame(door_frame, "wrist_roll_link");
-
-        //if (door_joint->getNumDofs() > 0)
-        //    fetch_dart->addNameToGroup(GROUP, "base_to_door");
-
-        auto myjoyts = fetch_dart->getGroupJoints(GROUP);
-        fetch_dart->processGroupWithJoint(GROUP,  *door_joint);
-
-        //fetch_dart->addNameToGroup(GROUP, "base_to_door");
-
-        
-        RBX_INFO("teeeeeeeeeeeeest");
-
-
+        auto door_jt = door_dart->getGroupJoints("dodoor");
+        //auto door_fr = door_dart->getFrame("door");
+        //fetch_dart->reparentFreeFrame(door_fr,"wrist_roll_link");
         builder.addGroup(fetch_name, GROUP);
+        //builder.addGroup(door_name,"dodoor");
+        builder.rspace->addGroupFromJoints(GROUP,door_jt);
+
         builder.setStartConfigurationFromWorld();
 
+        darts::TSR::Specification constr_spec2;
+        constr_spec2.setBase(door_name,"door");
+        constr_spec2.setTarget(fetch_name,"wrist_roll_link");
+        //constr_spec2.setNoZPosTolerance();
+        constr_spec2.setPoseFromWorld(world);
+        auto constraint_tsr2 = std::make_shared<darts::TSR>(world,constr_spec2);
+        builder.addConstraint(constraint_tsr2);
 
+        /*
+        darts::TSR::Specification con2_spec;
+        con2_spec.setFrame(fetch_name, "wrist_roll_link", "base_link");
+        con2_spec.setPoseFromWorld(world);
+        con2_spec.setNoXPosTolerance();
+        con2_spec.setNoYPosTolerance();
+
+        auto con2_tsr = std::make_shared<darts::TSR>(world, con2_spec);
+        builder.addConstraint(con2_tsr);
+        */
+        builder.initialize();
+
+
+        /*
         darts::TSR::Specification constr_spec;
         constr_spec.setFrame(fetch_name,"wrist_roll_link", "base_link");
         constr_spec.setPose(0.40, 0.27, 0.76,  // starting pos
                             0.5, -0.5, 0.5, 0.5);
 
-
         auto constraint_tsr = std::make_shared<darts::TSR>(world,constr_spec);
         builder.addConstraint(constraint_tsr);
 
+        */
 
-        builder.initialize();
         darts::TSR::Specification goal_spec;
         goal_spec.setFrame(fetch_name, "wrist_roll_link", "base_link");
-        goal_spec.setPose(0.27 , -0.4, 0.76,  // goal pos
+        goal_spec.setPose(0.78 , -0.25, 0.76,  // goal pos
                           0.0, 0.71, 0.0, -0.71);
 
 
         auto goal_tsr = std::make_shared<darts::TSR>(world, goal_spec);
-
         auto goal = builder.getGoalTSR(goal_tsr);
 
         builder.setGoal(goal);
@@ -226,9 +237,24 @@ int main(int argc, char **argv)
         builder.ss->setPlanner(rrt);
 
         builder.setup();
+
+        auto tsr_set = builder.constraint->getSet();
+        auto sdhf = tsr_set->solveWorld();
+        //configuration from the builder
+        Eigen::VectorXd positions(tsr_set->getWorldIndices().size());
+        tsr_set->getPositionsWorldState(positions);
+
+        // How to get the current error given the world
+        Eigen::VectorXd error(tsr_set->getDimension());
+        tsr_set->getErrorWorld(error);
+
+
+        auto tf = goal->getTSRSet()->solveWorld();
+
         goal->startSampling();
         ompl::base::PlannerStatus solved = builder.ss->solve(500.0);
         goal->stopSampling();
+
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         while(true){
