db8999b7b73236dd4a6fdb1547e0520022c9388b
servetb <servetbora9@hotmail.com>
2022-02-04 18:34:38 +0100

    save before new isopath

--- robowflex_extensions/isopath.cpp
+++ robowflex_extensions/isopath.cpp
@@ -8,6 +8,7 @@
 using namespace robowflex::darts;
 #include <fstream>
 
+
 #include <boost/math/special_functions/factorials.hpp>
 
 std::vector<std::vector<int>> PlanBuilder::findGroupedIndices(const ompl::base::State *const rfrom) {
@@ -77,6 +78,20 @@ std::vector<std::vector<int>> getDiffIndices(std::vector<double> s1,std::vector<
 
 }
 
+std::vector<int> getDiffIndicesWithoutGroup(std::vector<double> s1,std::vector<double> s2){
+
+    std::vector<int> diff_index;
+
+    for(int j =0; j< s1.size(); j++){
+        if(s1.at(j) != s2.at(j))
+            diff_index.push_back(j);
+    }
+    return diff_index;
+
+}
+
+
+
 
 bool PlanBuilder::getIntermediateState(const ompl::base::State *from,std::vector<double> to,
                                        ompl::base::State *state, std::vector<int> index_group){
@@ -92,24 +107,37 @@ bool PlanBuilder::getIntermediateState(const ompl::base::State *from,std::vector
     space->copyFromReals(state,s_new_v);
     if(space->equalStates(state,from))
         return false;
-
     return true;
 
 }
 
 
+void PlanBuilder::getIntermediateState(std::vector<double> from,std::vector<double> to,
+                                       ompl::base::State *state, std::vector<int> index_group){
+    std::vector<double> s_new_v;
+    ompl::base::State *s_new = space->allocState();
 
-ompl::geometric::PathGeometric PlanBuilder::pathIsolateStates(ompl::geometric::PathGeometric path){
+    s_new_v = from;
+    for(int l = 0; l< index_group.size(); l++){
+        s_new_v[index_group[l]] = to[index_group[l]];
+    }
+    space->copyFromReals(state,s_new_v);
+
+}
+
+
+void PlanBuilder::pathIsolateStates(ompl::geometric::PathGeometric &path)
+{
 
     std::vector<ompl::base::State *> &states = path.getStates();
     std::vector<std::vector<int>> groupIndices = findGroupedIndices(states[0]);
     std::vector<int> group_index = groupIndices.at(0); // TODO for more than one group (groupIndices alrdy. correct -> todo in getDiffIndices()
     //std::vector<int> group_index = {0,1};
-    ompl::geometric::PathGeometric path_new = ompl::geometric::PathGeometric(info);
+    ompl::geometric::PathGeometric path_new(info);
     std::vector<ompl::base::State *> &states_new = path_new.getStates();
 
     states_new.push_back(states[0]);
-    
+
     for(int i = 0; i < path.getStateCount()-1; i++)
     {
         std::vector<double> s_from,s_to;
@@ -128,7 +156,7 @@ ompl::geometric::PathGeometric PlanBuilder::pathIsolateStates(ompl::geometric::P
         do
         {
             std::vector<ompl::base::State *> temp_states;
-            ompl::base::State * start_state = space->allocState();
+            ompl::base::State * start_state = info->allocState();
             space->copyState(start_state, states_new.back());
             for(int j = 0; j< diff_indices.size(); j++)
             {
@@ -137,17 +165,7 @@ ompl::geometric::PathGeometric PlanBuilder::pathIsolateStates(ompl::geometric::P
                     std::cout << "ERR: no state could be created" << std::endl;
                     break;
                 }
-                /*
-                std::cout << "start" << std::endl;
-                space->printState(states[i]);
-                space->printState(states[i+1]);
-                space->printState(start_state);
-                space->printState(s_new);
-                std::cout <<"check motion " << info->checkMotion(start_state,s_new) << std::endl;
-                std::cout <<"diff size " << diff_indices.size() <<" , j : " << j <<  std::endl;
-                std::cout <<"isvalid " << info->isValid(s_new) << std::endl;
-                std::cout << "start equal : " << space->equalStates(s_new,states[i+1]) <<  std::endl;
-                */
+
                  if(info->checkMotion(start_state,s_new))
                 {
                     temp_states.push_back(s_new);
@@ -167,11 +185,12 @@ ompl::geometric::PathGeometric PlanBuilder::pathIsolateStates(ompl::geometric::P
             if(iso_success){
                 isolated_sub_path=temp_states;
             }else if(valid_path_check < counter){
+                // TODO OKAY, WE GO ONE STEP BACK AND INTERPOLATE TO THE POINT AGAIN
                 RBX_WARN("PATH HAS INVALID STATES OR IS DAMAGED. TRIED ALL COMBINATIONS BUT NO ISOLATION SEQUENCE WAS FOUND.");
                 std::cout << "ABORT" << std::endl;
-                return path_new;
+                return;
             }
-            space->freeState(start_state);
+            info->freeState(start_state);
         }while(std::next_permutation(diff_indices.begin(),diff_indices.end()),iso_success!=true);
 
         states_new.insert(states_new.end(),isolated_sub_path.begin(),isolated_sub_path.end());  // add the founded sub-isolated path
@@ -181,9 +200,169 @@ ompl::geometric::PathGeometric PlanBuilder::pathIsolateStates(ompl::geometric::P
     std::ofstream fs("maze_sol_pathiso.txt");
     path_new.printAsMatrix(fs);
     std::cout << "isolated path" << std::endl;
-    return path_new;
+
+    path.clear();
+    path = path_new;
+
+
+    //path_new.clear();
+    //return *path_new;
 
 }
+
+std::vector<std::vector<double>> PlanBuilder::getPathReals(std::vector<ompl::base::State *> states)
+{
+    std::vector<std::vector<double>> state_reals;
+
+    for(int i = 0; i < states.size(); i++)
+    {
+        std::vector<double> s_;
+        space->copyToReals(s_,states[i]);
+        state_reals.push_back(s_);
+    }
+    return state_reals;
+
+}
+
+bool goalStateReached(std::vector<double> goal_, std::vector<double> curr_, std::vector<int> group_index)
+{
+   bool reach = true;
+    for(int i = 0; i < group_index.size(); i++)
+    {
+        if(goal_.at(i) != curr_.at(group_index.at(i)))
+            return false;
+    }
+    return reach;
+}
+
+std::vector<double> extractGoal(std::vector<double> goal_state, std::vector<int> group_index){
+   std::vector<double> goal_values;
+    for(int i = 0; i < group_index.size(); i++)
+    {
+        goal_values.push_back(goal_state.at(group_index.at(i)));
+    }
+    return goal_values;
+}
+
+void mergeGroupsMLQ(std::vector<std::vector<int>> &MLQ, int states_len, std::vector<int> group_index){
+    std::vector<std::vector<int>> groupQs;
+    for(auto &index : group_index)
+        groupQs.push_back(MLQ.at(index));
+
+    std::vector<int> merge;
+
+    int counter1=0;
+    int counter2 = 0;
+    for(int i = 0; i < states_len-1; i++){
+        bool flag = false;
+        if(counter1 < groupQs.at(0).size() && groupQs.at(0).at(counter1) == i){
+            merge.push_back(i);
+            flag = true;
+            counter1++;
+        }
+        if(counter2 < groupQs.at(1).size() && groupQs.at(1).at(counter2) == i){
+            if(!flag)
+                merge.push_back(i);
+            counter2++;
+        }
+    }
+
+    std::vector<std::vector<int>> newMLQ;
+    newMLQ.push_back(merge);
+    for(int i = 2; i < MLQ.size(); i++)
+        newMLQ.push_back(MLQ.at(i));
+
+    MLQ = newMLQ;
+}
+
+ompl::geometric::PathGeometric PlanBuilder::optimizeIsolatedPath(ompl::geometric::PathGeometric path)
+{
+
+    std::vector<ompl::base::State *> &states = path.getStates();
+    std::vector<std::vector<int>> groupIndices = findGroupedIndices(states[0]);
+    std::vector<int> group_index = groupIndices.at(0); // TODO group indices as key
+    //std::vector<int> group_index = {0,1};
+    ompl::geometric::PathGeometric path_new = ompl::geometric::PathGeometric(info);
+    std::vector<ompl::base::State *> &states_new = path_new.getStates();
+
+    // TODO SET CUBE AS KEY GROUP, PRIORITY QUEUE
+
+    states_new.push_back(states[0]);
+
+    std::vector<std::vector<double>> state_reals = getPathReals(states);
+    std::vector<double> goal_values = extractGoal(state_reals.back(),group_index);
+     // goalvalues now : -0.05 , 0.55 rest egal
+    std::vector<double> state_it = state_reals.front();
+     //func check goal reached
+
+     std::vector<std::vector<int>> MLQ; // priority MLQ, cube is always put front, other
+     std::vector<int> cubegr,cubegr2,door1,door2,door3;
+     MLQ.push_back(cubegr);
+     MLQ.push_back(cubegr2);
+     MLQ.push_back(door1);
+     MLQ.push_back(door2);
+     MLQ.push_back(door3);
+
+     for(int i = 0; i < state_reals.size()-1 ; i++)
+     {
+         std::vector<int> diff_indices = getDiffIndicesWithoutGroup(state_reals[i],state_reals[i+1]);
+         for(int j = 0; j < diff_indices.size(); j++)
+             MLQ.at(diff_indices.at(j)).push_back(i);
+
+         //wrote indices in states reals for all states that were changed
+     }
+
+     // MLQ ready now plan to simplify and schedule tasks
+     //TODO merge MLQ
+     mergeGroupsMLQ(MLQ,state_reals.size(),group_index);
+     int iterator = 0; // iterator zeigt immer auf 0 bzw cube wenns nicht klappt zum nächsten ++ usw, danach wird nochmal 0 wenn cube erfolgreich bewegt werden konnte
+/*
+    while(!MLQ.at(0).empty())
+     {
+
+         ompl::base::State * s1 = space->allocState();
+         space->copyFromReals(s1,state_reals.at(MLQ.at(0).front()));
+         ompl::base::State * s2 = space->allocState();
+         space->copyFromReals(s2,state_it);
+
+
+
+        if(!checkmotion)
+            counter++
+
+         if(goalStateReached(goal_values,state_it,group_index))
+             break;
+     }
+
+    std::ofstream fs("maze_sol_pathiso.txt");
+    path_new.printAsMatrix(fs);
+    std::cout << "isolated path" << std::endl;
+    return path_new;
+*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
